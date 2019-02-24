b89618e97ae0106cfc2fb557306732f2308d8609
Brayden Phillips <bpofficial@icloud.com>
2019-02-24 19:40:52 +1000

    Created reverseGeo as an external function

---
+++ src/Modules/Shared/Geo/index.tsx
@@ -0,0 +1,23 @@
+import { store } from "../../index";
+
+export const reverseGeo = (data, google, createMarker: boolean) => {
+    const Geo = new google.Geocoder();
+    const location = {
+        address: data.description,
+        lat: 0,
+        lng: 0,
+        place_id: data.place_id,
+    };
+    return new Promise((resolve, reject) => {
+        Geo.geocode({placeId: data.place_id}, (results, status) => {
+            if (status === "OK") {
+                location.lat = results[0].geometry.location.toJSON().lat;
+                location.lng = results[0].geometry.location.toJSON().lng;
+                if (createMarker) { store.dispatch({type: "NEW_MARKER", data: location}); }
+                resolve(location);
+            } else {
+                reject(new Error("Had some trouble."));
+            }
+        });
+    });
+};
