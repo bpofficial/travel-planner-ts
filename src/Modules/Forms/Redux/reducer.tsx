4eb202396fe4f426b72cdd1387cc197081de7d2b
Brayden Phillips <bpofficial@icloud.com>
2019-02-24 20:22:09 +1000

    Added `...state,` to fix error arrising from not spreading other values

--- src/Modules/Forms/Redux/reducer.tsx
+++ src/Modules/Forms/Redux/reducer.tsx
@@ -1,7 +1,18 @@
-const formMap = (state = {focus: {}, editing: false, click: null}, action) => {
+const initState = {
+    click: undefined,
+    editing: false,
+    focus: {
+        description: "",
+        lat: 0,
+        lng: 0,
+    },
+};
+
+const formMap = (state = initState, action) => {
     switch (action.type) {
         case "NEW_MARKER":
             return {
+                ...state,
                 focus: {
                     description: action.data.address,
                     lat: action.data.lat,
@@ -11,7 +22,7 @@ const formMap = (state = {focus: {}, editing: false, click: null}, action) => {
         case "MAP_CLICK":
             return {
                 ...state,
-                click: state.editing ? action.event : null,
+                click: state.editing ? action.event : undefined,
             };
         case "EDITING":
             return {
